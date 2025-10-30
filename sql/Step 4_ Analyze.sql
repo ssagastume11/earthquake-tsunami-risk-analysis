2d0b6296dce8757b8a52d020e048d6b2545acdde
ssagastume11 <sagassergio123@gmail.com>
2025-10-30 11:09:46 -0400

    BigQuery SQL queries

--- sql/Step 4_ Analyze.sql
+++ sql/Step 4_ Analyze.sql
@@ -1,1 +1,10 @@
+-- Objective:
+-- This goal of this step is to explore and analyze the cleaned earthquake dataset to uncover patterns, trends, and relationships between earthquake characteristics and tsunami occurrences from 2001-2022.
 
+-- This includes examing how magnitude, depth, and location influence the likelihood of tsunamis and identifying regions with higher seismic activity or risk.
+
+-- Key Actions:
+-- * Calculating yearly and monthly trends in earthquake frequency and magnitude.
+-- * Identifying correlations between magnitude, depth, and tsunami classification.
+-- * Analyzing geographic patterns of earthquakes and tsunami-related events.
+-- * Preparing summary statistics for visualization and interpretation.
