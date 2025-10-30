3257f4f1d7cd06717ae1e339762688ebf16825ac
ssagastume11 <sagassergio123@gmail.com>
2025-10-30 11:10:10 -0400

    BigQuery SQL queries

--- sql/Step 5_ Share.sql
+++ sql/Step 5_ Share.sql
@@ -1,1 +1,8 @@
+-- Objective:
+-- The goal of this step is to visualize and communicate insights from the earthquake-tsunami dataset.
+-- This includes indentifying yearly patterns, magnitude-depth relationships, and how tsunami classifications vary across time.
 
+-- Key Actions:
+-- * Use BigQuery charts or export to Tableau for trend visualization,
+-- * Plot yearly average magnitude and depth trends.
+-- * Highlight the frequency of tsunamis versus non-tsunamis per year.
