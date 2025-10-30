09deb4ccf767d79890a02151dfc4a3b3637646e1
ssagastume11 <sagassergio123@gmail.com>
2025-10-30 11:09:28 -0400

    BigQuery SQL queries

--- sql/Step 3_ Process.sql
+++ sql/Step 3_ Process.sql
@@ -1,1 +1,10 @@
+-- Objective:
+-- The goal of this step is to process and transform the cleaned earthquake-tsunami dataset to prepare for meaningful analysis and visualizations.
+-- This includes creating calculating fields, filtering data by revelant criteria, and deriving new insights such as average magnitudes, event frequencies and tsunami occurrence patterns across year and regions.
 
+-- Key Actions:
+-- * Filtering out low-magnitude (below 6.5) to focus on sighnificant seismic events.
+-- * Creating new calculated fields to capture relationships between magnitude, depth, and tsunami occurrence.
+-- * Summarizing data by year, month, and location to identify temporal and geigraphic trends.
+-- * Aggregating statistics such as mean magnitude, total number of tsunami-related events, and percentage of tsunami occurrences.
+-- * Ensuring processed data maintains integrity for accurate risk analysis and visualizations in later steps.
