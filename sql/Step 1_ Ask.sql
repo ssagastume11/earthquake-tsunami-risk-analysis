d56d21a54e45f961f3f6c8a320b13e6153463f54
ssagastume11 <sagassergio123@gmail.com>
2025-10-30 11:08:37 -0400

    BigQuery SQL queries

--- sql/Step 1_ Ask.sql
+++ sql/Step 1_ Ask.sql
@@ -1,1 +1,9 @@
+-- Business Task:
+-- The goal of this project is to analyze global earthquake data from 2001-2022 to understand seismic behavior and assess tsunami risks. The insights will support early warning systems, hazard mapping, and help improve preparedness for tsunami-prone regions.
 
+-- Key Questions:
+-- 1. Which regions experienced the highest frequency of tsunami-generating earthquakes?
+-- 2. How does earthquake magnitude relate to tsunami occurrence?
+-- 3. Is there a correlation between earthquake depth and tsunami risk?
+-- 4. What temporal trends (yearly or seasonal) are observable in seismic and tsunami activity?
+-- 5. How balanced is the dataset in terms of tsunami vs. non-tsunami events?
